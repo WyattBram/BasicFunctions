7338a8884c77c2438679fdd01e6889b04ad37cc1
WyattBram <143138924+WyattBram@users.noreply.github.com>
2023-10-06 14:25:30 -0400

    Update Soultion.java

--- Soultion.java
+++ Soultion.java
@@ -4,7 +4,7 @@ public class Soultion {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = nums.length -1; j > 0; j--) {
-                if (nums[i] + nums[j] == target){
+                if (nums[i] + nums[j] == target && i!=j){
                     return new int[]{i,j};
                 }
             }
